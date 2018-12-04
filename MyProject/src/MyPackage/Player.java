ea34e52ecf4a38a4806401fbb18fcae220b56683
aymericdelab <aymericdelabrousse@gmail.com>
2018-12-04 23:52:46 +0100

    Ajoute plusieurs méthodes qui diminuent ou augmentent les skills ou energie, et ajout de limitations sur les mouvements du personnage

--- MyProject/src/MyPackage/Player.java
+++ MyProject/src/MyPackage/Player.java
@@ -1,14 +1,19 @@
 package MyPackage;
 
+import java.awt.SystemTray;
+
 public class Player {
-		
+	
+	//username of the player, can be used later for the highscores
+	String username;
+	
 	//player position
 	int x_pos;
 	int y_pos;
 	
-	// skill attributs
+	// skill attributes 
 	
-	int looks;
+	static int looks = 40;
 	int danceSkills;
 	
 	// energy
@@ -19,14 +24,14 @@ public class Player {
 		x_pos = 0;
 		y_pos = 0;
 		
-		//The neutral value for looks and dancing skills is set to 30 and the value changes
+		//The neutral value for looks and dancing skills is set to 20 and the value changes
 		//with the value that the player inputs on a scale that goes from looks to dancing skills.
 		//If the player favors the left side of the scale, the looks will increase and the dancing
 		//will decrease. The scale goes from 0 (completely favors the looks) to 10 (completely 
-		//favors the dancing skills.
+		//favors the dancing skills).
 		
-		looks = 30 + (10 - input_skills);
-		danceSkills = 30 - input_skills;
+		looks = 20 + 2*(10 - input_skills);
+		danceSkills = 20 + 2*input_skills;
 		
 		//The neutral value for the energy is set to 30 and decreases as we increase the difficulty
 		//times two
@@ -34,14 +39,58 @@ public class Player {
 		
 	}
 	
-	void move(int x1, int y1) {
-		x_pos = x_pos + x1;
-		y_pos = y_pos + y1;
+	//method to move the player in any direction but maximum of 1 step
+	 void move(int x1, int y1) {
+		 
+		 if(Math.abs(x1)>1 || (Math.abs(y1)>1)) {
+				System.out.println("Unreachable");
+			}
+		else {
+			x_pos = x_pos + x1;
+			y_pos = y_pos + y1;
+			
+		}
+	 }
+			
+	void gainEnergy() {
+		energy++;
 	}
-	
+	 
 	void loseEnergy() {
 		
 		energy--;
 	}
+	
+	void loseDancingSkills() {
+		
+		danceSkills--;
+	}
+	
+	void gainDancingSkills() {
+		
+		danceSkills++;
+	}
+	
+	void loseLooks() {
+		
+		looks--;
+	}
+	
+	void gainLooks() {
+		
+		looks++;
+	}	
+	
+	//int is the type of return of this method, if not return then void
+	
+	
+	int getDance_skills() {
+		return danceSkills;
+	}
 
-}
+	int getLooks() {
+		return looks;
+	}
+}	
+
+
--- MyProject/src/MyPackage/Program.java
+++ MyProject/src/MyPackage/Program.java
@@ -1,15 +1,17 @@
 package MyPackage;
 
+
 public class Program {
+
 	
+	//main method is the first method that the program will run
 	public static void main(String[] args) {
 		
-		//create new player
-		Player player1 = new Player(0,0);
-		
-		System.out.println("The energy level is "+player1.energy+", the look skills are set to "
-				+ player1.looks+ " and the dancing skills are set to "+ player1.danceSkills);
+	Player player1 = new Player(0,0);
 	
+	player1.move(2, 1);
+	
+	System.out.println(player1.x_pos + ", " + player1.y_pos);
 			
 	}
 
