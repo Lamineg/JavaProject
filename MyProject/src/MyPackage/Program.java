3745c99ab7e372640b209d7049448e4b1dd8fdb7
aymericdelab <aymericdelabrousse@gmail.com>
2018-12-04 11:36:11 +0100

    test

--- MyProject/src/MyPackage/Program.java
+++ MyProject/src/MyPackage/Program.java
@@ -4,11 +4,12 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
+		//create new player
 		Player player1 = new Player();
 		
 		System.out.println("x = "+player1.x+", y = "+ player1.y);
 		
-		player1.move(1,5);
+		player1.move(1,4);
 		
 		System.out.println("x = "+player1.x+", y = "+ player1.y);
 		
