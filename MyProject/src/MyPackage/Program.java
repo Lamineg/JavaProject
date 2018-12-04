98f84dadc4b4e50dc9314eb0fa4904dcdfad2d5d
Lamineg <guillaume.lamine@student.uclouvain.be>
2018-12-04 12:29:33 +0100

    fin de fusion

--- MyProject/src/MyPackage/Program.java
+++ MyProject/src/MyPackage/Program.java
@@ -4,11 +4,12 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
+		//create new player
 		Player player1 = new Player();
 		
 		System.out.println("x = "+player1.x_pos+", y = "+ player1.y_pos);
 		
-		player1.move(1,5);
+		player1.move(1,3);
 		
 		System.out.println("x = "+player1.x_pos+", y = "+ player1.y_pos);
 		
