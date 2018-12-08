3c1dfe1cb565b5a754a59c88dc56e54983d40694
Lamineg <guillaume.lamine@student.uclouvain.be>
2018-12-08 15:48:55 +0100

    change main

--- MyProject/src/MyPackage/Grid.java
+++ MyProject/src/MyPackage/Grid.java
@@ -5,6 +5,9 @@ public class Grid {
 	int x_dim = 5;
 	int y_dim = 5;
 	
+	int trophy_x = 3;
+	int trophy_y = 4;
+	
 	BlindSpot myBlindSpot = new BlindSpot();
 	Juror myJuror;
 	SlipperyTile mySlipTile;
--- MyProject/src/MyPackage/Program.java
+++ MyProject/src/MyPackage/Program.java
@@ -17,7 +17,14 @@ public class Program {
 		
 		Grid myGrid = new Grid();
 		
+		int [] positions_x = {2,2,-1};//new int [4];
+		int [] positions_y = {3,-1,1};//new int [4];
+		
 		System.out.println("blind x = "+myGrid.myBlindSpot.x+", blind y = "+ myGrid.myBlindSpot.y);
+		
+		while (player1.energy > 0 || (myGrid.trophy_x == player1.x_pos && myGrid.trophy_y == player1.y_pos)) {
+			
+		}
 		
 	}
 
