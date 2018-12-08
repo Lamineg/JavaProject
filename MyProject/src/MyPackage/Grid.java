8a75d93dcf6d597a0eac593b70de6a1456f59a4f
Lamineg <guillaume.lamine@student.uclouvain.be>
2018-12-08 18:33:41 +0100

    enhance grid constructor + add randomness

--- MyProject/src/MyPackage/Grid.java
+++ MyProject/src/MyPackage/Grid.java
@@ -3,14 +3,22 @@ package MyPackage;
 public class Grid {
 	
 
-	int x_dim = 5;
-	int y_dim = 5;
+	int x_dim;// = 5;
+	int y_dim;// = 5;
 	
-	int trophy_x = 3;
-	int trophy_y = 3;
+	int trophy_x;// = 3;
+	int trophy_y;// = 3;
 	
-	BlindSpot myBlindSpot = new BlindSpot();
+	BlindSpot myBlindSpot;// = new BlindSpot();
 	Juror myJuror;
 	SlipperyTile mySlipTile;
+	
+	Grid(int x, int y){
+		x_dim = x;
+		y_dim = y;
+		
+		trophy_x = (int) (Math.random()*(x + 1));
+		trophy_y = (int) (Math.random()*(y + 1));
+	}
 
 }
--- MyProject/src/MyPackage/Program.java
+++ MyProject/src/MyPackage/Program.java
@@ -15,28 +15,47 @@ public class Program {
 		
 		// TEST FOR GRID PART :
 		
-		Grid myGrid = new Grid();
+		Grid myGrid = new Grid(5,5);
 		
 		int [] positions_x = {2,2,-1};//new int [4];
 		int [] positions_y = {3,-1,1};//new int [4];
 		
-		System.out.println("blind x = "+myGrid.myBlindSpot.x+", blind y = "+ myGrid.myBlindSpot.y);
+		//System.out.println("blind x = "+myGrid.myBlindSpot.x+", blind y = "+ myGrid.myBlindSpot.y);
 		
 		//
 		
 		System.out.println("energy = "+player1.energy);
 		
+		double r = Math.random()*4;
+		int u = (int) r;
+		System.out.println("u = " + u);
+		System.out.println("r = " + r);
+		
+		System.out.println("trophy x = "+myGrid.trophy_x+", trophy y = "+ myGrid.trophy_y);
 		
 		int i = 0;
+		int movX, movY;
+		
 		while (player1.energy > 0 && (myGrid.trophy_x != player1.x_pos || myGrid.trophy_y != player1.y_pos)) {
 			
-			player1.move(positions_x[i], positions_y[i]);
+			//player1.move(positions_x[i], positions_y[i]);
+			
+			movX = (int) (Math.random()*(4 + 4 + 1)) - 4;
+			movY = (int) (Math.random()*(4 + 4 + 1)) - 4;
+			
+			player1.move(movX, movY);
+			
+			if(player1.x_pos > 4) player1.x_pos = 4;
+			if(player1.x_pos < 0) player1.x_pos = 0;
+			if(player1.y_pos > 4) player1.y_pos = 4;
+			if(player1.y_pos < 0) player1.y_pos = 0;
+			
 			System.out.println("x = "+player1.x_pos+", y = "+ player1.y_pos);
 			
 			if (myGrid.trophy_x == player1.x_pos && myGrid.trophy_y == player1.y_pos) {
 				System.out.println("You found the trophy !");
 			}
-			if (player1.energy < 0) {
+			if (player1.energy <= 0) {
 				System.out.println("You run out of energy :'( ");
 			}
 			i++;
