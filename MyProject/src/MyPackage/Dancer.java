1eea81efdde774e5138cc1aa1f01c282cda3eabf
aymericdelab <aymericdelabrousse@gmail.com>
2018-12-20 15:19:08 +0100

    commentaires

--- MyProject/src/MyPackage/Dancer.java
+++ MyProject/src/MyPackage/Dancer.java
@@ -2,16 +2,19 @@ package MyPackage;
 
 public class Dancer {
 
+	//initializes variables for the position and threshold of the dancer
 	private int x;
 	private int y;
 	private int danceThreshold;
 
+	//creates an object dancer
 	Dancer(int x, int y, int id, int danceThreshold) {
 		this.x = x;
 		this.y = y;
 		this.danceThreshold = danceThreshold;
 	}
-
+	
+	//getters
 	public int getX() {
 		return x;
 	}
--- MyProject/src/MyPackage/FirstScreen.java
+++ MyProject/src/MyPackage/FirstScreen.java
@@ -17,42 +17,67 @@ import javax.swing.JTextField;
 
 public class FirstScreen {
 
+	
 	private Object lock = new Object();
 	private static String username;
 	private static int difficulty;
 	private static int input_skills;
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-
+	
+	//this method will launch the first GUI window that will ask some info to the player,
+	// the username, the difficulty and what skills he favors
 	public void run() throws InterruptedException {
-
+		
+		//creates a panel and sets his layout as a BoxLayout
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		// panel.setLayout(new FlowLayout(FlowLayout.CENTER));
-
+		
+		//add the panel to the frame
 		frame.add(panel);
 
+		//creates a field for the player to write his username
 		JTextField text = new JTextField(15);
 		text.setSize(20, 10);
+		
+		//label 'username' for the text field
 		JLabel labeluser = new JLabel("Username: ");
+		
+		//add both to the panel
 		panel.add(labeluser);
 		panel.add(text);
 
+		//creates a slider for the difficulty
 		JSlider diff = new JSlider(0, 10, 5);
-		JSlider skill = new JSlider(0, 10, 5);
 
+		//creates a title for the difficulty slider
 		JLabel diff_label = new JLabel("Difficulty: ");
+		
+		//attach the title to the difficulty slider
 		diff_label.setLabelFor(diff);
+		
+		//add both to the panel
 		panel.add(diff_label);
 		panel.add(diff);
-
+		
+		//creates a slider for the skills
+		JSlider skill = new JSlider(0, 10, 5);
+		
+		//create a title for the skill slider
 		JLabel skill_label = new JLabel("Skills: ");
+		
+		//attach the title to the skill slider
 		skill_label.setLabelFor(skill);
+		
+		//add both to the panel
 		panel.add(skill_label);
 		panel.add(skill);
 
+		//true because we want to display a label for each value of the sliders as well
 		diff.setPaintLabels(true);
 		skill.setPaintLabels(true);
-
+		
+		//create a table to store the number that will be setup under each value
+		//the difficulty slider
 		Hashtable<Integer, JLabel> table1 = new Hashtable<Integer, JLabel>();
 
 		for (int i = 0; i < 11; i++) {
@@ -60,36 +85,53 @@ public class FirstScreen {
 			table1.put(i, new JLabel(String.valueOf(i)));
 
 		}
-
+		
+		//attach the numbers to the slider
 		diff.setLabelTable(table1);
-
+		
+		
+		//creates a second table for the labels under each of the values of 
+		//the skill slider. We don't put anything for the labels except from the extremities 
 		Hashtable<Integer, JLabel> table2 = new Hashtable<Integer, JLabel>();
 		for (int i = 0; i < 11; i++) {
 
-			table2.put(i, new JLabel(String.valueOf(i)));
+			table2.put(i, new JLabel(""));
 
 		}
-
+		
+		//extremities of the slider 
 		table2.put(10, new JLabel("Dancing Skills"));
 		table2.put(0, new JLabel("Looks"));
 
+		//attach the labels to the slider
 		skill.setLabelTable(table2);
-
+		
+		//new button
 		JButton enter = new JButton("ENTER");
+		
+		//label for the enter button instructions for the information of the player 
+		//to be validated
 		JLabel instructions = new JLabel("Click ENTER and exit the window to validate");
 		instructions.setLabelFor(enter);
 
+		//attach the label to the enter button
 		panel.add(instructions);
+		
+		//add the enter button to the panel
 		panel.add(enter);
 
+		//size of the frame
 		frame.setSize(400, 220);
 		frame.setVisible(true);
-
+		
+		
+		//creates an actionlistener on the enter button so that we perform some actions when pressed
 		enter.addActionListener((new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				
+				//saves the username written in the text field
 				setUsername(text.getText().toString());
 				if (validUserName(username) == true) {
 					JOptionPane.showMessageDialog(frame, "Invalid username: at least 3 characters and no spaces!");
